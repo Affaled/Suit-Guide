3b4bb66396c383d45d4725840c3592f9e96ccc17
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-14 06:25:40 -0300

    App create

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/home/Home";
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
