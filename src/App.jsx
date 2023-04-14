c8435dd98486a292552aebbaf78b988b0e7b4130
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-14 06:45:00 -0300

    routes create && config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
 import React from "react";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Home />
+      <Header />
+      <Outlet />
     </div>
   );
 }
