ed865edc646ec680b3e5b989ec5071841759aed3
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-14 21:51:59 -0300

    home mod

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import "./Home.css";
+import photo from "../../assets/images/home-photo.jpg";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <img src={photo} alt="Main Photo" className="photo" />
+      <div className="text">
+        <h1>Men's Suit Guide</h1>
+        <h2>Tips for Always Looking Elegant and Appropriate</h2>
+        <p>
+          Dressing appropriately can convey respect, professionalism, and
+          confidence. Therefore, choosing the right tuxedo for every occasion is
+          so important. Our website provides tips on fit, measurements, and
+          colour combinations to ensure that you're always dressed elegantly and
+          appropriately.
+        </p>
+        <p>
+          Whether it's for a job interview, wedding, or any other formal
+          occasion, our comprehensive tuxedo guide is here to help you stand
+          out. Explore all the information we have available and make sure
+          you're always dressed in accordance with the situation.
+        </p>
+      </div>
+    </div>
+  );
 }
