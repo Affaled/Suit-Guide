9bb993b9d36af677005af9850c49eaf48144cb12
Affaled <120531449+Affaled@users.noreply.github.com>
2023-08-07 20:47:16 -0300

    definição das janelas principais

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function App() {
-  return <div className="App">teste</div>;
+  return (
+    <>
+      <div className="drawer">a</div>
+      <div className="window">
+        <header className="window-header"></header>
+        <main className="window-main"></main>
+        <footer className="window-footer"></footer>
+      </div>
+    </>
+  );
 }
