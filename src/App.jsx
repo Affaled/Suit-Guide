3754881ee22db142aab4bc5f410c5abf9eb35925
Affaled <120531449+Affaled@users.noreply.github.com>
2023-08-08 16:09:53 -0300

    recomeço

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return (
-    <>
-      <div className="drawer">a</div>
-      <div className="window">
-        <header className="window-header"></header>
-        <main className="window-main"></main>
-        <footer className="window-footer"></footer>
-      </div>
-    </>
-  );
+  return <></>;
 }
