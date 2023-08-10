afc6ebc6d45aecc677c90ebb7149caa8e9d785e4
Affaled <120531449+Affaled@users.noreply.github.com>
2023-08-10 15:17:45 -0300

    Adição do conteúdo drawer

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function Drawer() {
-  return <div>Drawer</div>;
+  return (
+    <aside className="Drawer">
+      <nav>
+        <ul>
+          <li>
+            <a href="a">Início</a>
+          </li>
+          <li>
+            <a href="a">Estoque</a>
+          </li>
+          <li>
+            <a href="a">Busca</a>
+          </li>
+          <li>
+            <a href="a">Pedidos</a>
+          </li>
+          <li>
+            <a href="a">Histórico</a>
+          </li>
+          <li>
+            <a href="a">Relatórios</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
