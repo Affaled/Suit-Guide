654c78cd1b9b130e1733559c945569111208a585
Affaled <120531449+Affaled@users.noreply.github.com>
2023-08-11 17:49:11 -0300

    criação e aplicação do header, ajuste compatibilidade estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Drawer from "./components/Drawer";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
       <Drawer />
+      <Header />
     </>
   );
 }
