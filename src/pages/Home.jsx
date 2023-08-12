45499daee6d9eb7aa12a13a650b62a876cd20b29
Affaled <120531449+Affaled@users.noreply.github.com>
2023-08-12 16:43:14 -0300

    adicionada área do inventário

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,20 @@ import React from "react";
 export default function Home() {
   return (
     <main className="Home">
-      <div className="Home__inventory">teste</div>
+      <div className="Home__inventory">
+        <span
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            paddingBottom: "1rem",
+          }}>
+          <div className="Home__inventory--revenue">teste</div>
+          <div className="Home__inventory--expences">teste</div>
+          <div className="Home__inventory--profit">teste</div>
+          <div className="Home__inventory--profitToday">teste</div>
+        </span>
+        <div className="Home__inventory--list">teste</div>
+      </div>
     </main>
   );
 }
